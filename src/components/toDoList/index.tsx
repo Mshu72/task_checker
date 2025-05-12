f46d9990a59dedf6ecb90272921d6f9d29d6bbc4
Mshu72 <m.shu.720@gmail.com>
2025-05-12 22:45:41 +0900

    add modal2

--- src/components/toDoList/index.tsx
+++ src/components/toDoList/index.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React,{ useState} from "react";
 import { Task } from "../task"
+import { FormModal } from "../modal";
 import MenuIcon from "@mui/icons-material/Menu";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import "./style.css";
 
 export const ToDoList = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
   return (
     <div className="task_list">
         <div className="section">
@@ -13,7 +21,13 @@ export const ToDoList = () => {
           <AddCircleOutlineIcon
             className="add_circle_outline_icon"
             fontSize="small"
+            onClick={handleOpen}
           />
+          <FormModal
+          handleClose={handleClose}
+          isOpen={isOpen}
+          body="taskBody"
+        />
         </div>
       <div className="task_field">
         <Task />
